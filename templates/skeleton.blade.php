f7d50064f5048462d4e404f339ae284b7c8a47df
Pendi Setiawan <pendi@Pendis-MacBook-Pro.local>
2017-08-08 09:27:05 +0700

    change logo and favicon

--- templates/skeleton.blade.php
+++ templates/skeleton.blade.php
@@ -14,11 +14,30 @@
 		</title>
 		<meta name="description" content="{{ f('page.title', 'Great App') }}">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="shortcut icon" href="<?php echo Theme::base('img/favicon.ico') ?>" type="image/x-icon">
 
 		<link href="<?php echo Theme::base('css/app.min.css') ?>" rel="stylesheet">
 		<link rel="stylesheet" href="<?php echo Theme::base('vendor/bootstrap/dist/css/bootstrap.min.css')?>">
 		<link href="<?php echo Theme::base('css/style.css') ?>" rel="stylesheet">
+
+		<!-- favicon -->
+		<link rel="shortcut icon" href="<?php echo Theme::base('img/event-favicon/favicon.ico') ?>" type="image/x-icon">
+		<link rel="apple-touch-icon" sizes="57x57" href="<?php echo Theme::base('img/event-favicon/apple-icon-57x57.png') ?>">
+		<link rel="apple-touch-icon" sizes="60x60" href="<?php echo Theme::base('img/event-favicon/apple-icon-60x60.png') ?>">
+		<link rel="apple-touch-icon" sizes="72x72" href="<?php echo Theme::base('img/event-favicon/apple-icon-72x72.png') ?>">
+		<link rel="apple-touch-icon" sizes="76x76" href="<?php echo Theme::base('img/event-favicon/apple-icon-76x76.png') ?>">
+		<link rel="apple-touch-icon" sizes="114x114" href="<?php echo Theme::base('img/event-favicon/apple-icon-114x114.png') ?>">
+		<link rel="apple-touch-icon" sizes="120x120" href="<?php echo Theme::base('img/event-favicon/apple-icon-120x120.png') ?>">
+		<link rel="apple-touch-icon" sizes="144x144" href="<?php echo Theme::base('img/event-favicon/apple-icon-144x144.png') ?>">
+		<link rel="apple-touch-icon" sizes="152x152" href="<?php echo Theme::base('img/event-favicon/apple-icon-152x152.png') ?>">
+		<link rel="apple-touch-icon" sizes="180x180" href="<?php echo Theme::base('img/event-favicon/apple-icon-180x180.png') ?>">
+		<link rel="icon" type="image/png" sizes="192x192"  href="<?php echo Theme::base('img/event-favicon/android-icon-192x192.png') ?>">
+		<link rel="icon" type="image/png" sizes="32x32" href="<?php echo Theme::base('img/event-favicon/favicon-32x32.png') ?>">
+		<link rel="icon" type="image/png" sizes="96x96" href="<?php echo Theme::base('img/event-favicon/favicon-96x96.png') ?>">
+		<link rel="icon" type="image/png" sizes="16x16" href="<?php echo Theme::base('img/event-favicon/favicon-16x16.png') ?>">
+		<link rel="manifest" href="<?php echo Theme::base('img/event-favicon/manifest.json') ?>">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="<?php echo Theme::base('img/event-favicon/ms-icon-144x144.png') ?>">
+		<meta name="theme-color" content="#ffffff">
 
 		@section('customcss')
 			<!-- Custom CSS -->
@@ -61,12 +80,12 @@
 								@section('applogo')
 									<span class="logo">
 										@section('applogo.image')
-											<img src="<?php echo Theme::base('img/bono.png') ?>" alt="Bono" />
+											<img src="<?php echo Theme::base('img/event_logo.png') ?>" alt="Bono" />
 										@show
 									</span>
 									<span>
 										@section('applogo.title')
-											Bono
+											Event
 										@show
 									</span>
 								@show
@@ -147,18 +166,14 @@
 								</li>
 								<li class="sub user">
 									<a href="#">
-										<span class="photo" style="background-image: url('http://lorempixel.com/200/200/people');"></span>
+										<!-- <span class="photo" style="background-image: url('http://lorempixel.com/200/200/people');"></span> -->
 										<span>Admin</span>
 										<i class="xn xn-down-open-mini"></i>
 									</a>
-									<ul class="context-menu higher right">
-										<li><a href="<?php echo URL::site('logout')?>">LogOut</a></li>
-										
-										
+									<ul class="context-menu higher right" style="padding: 0px">
+										<li><a href="<?php echo URL::site('logout')?>">LogOut</a></li>	
 									</ul>
 								</li>
-								<li class="devider"></li>
-								<li><a href="#"><i class="xn xn-help-circled"></i></a></li>
 							</ul>
 						@show
 					</div>
